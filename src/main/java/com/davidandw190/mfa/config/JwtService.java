d939130f8913b4d0b2a036de818ff914c8dcfbd7
Andrei-David Nan <nandavid190@gmail.com>
2023-08-08 17:53:49 +0300

    v0.4 -  set up extractClaim generic method and some of the related methods;

--- src/main/java/com/davidandw190/mfa/config/JwtService.java
+++ src/main/java/com/davidandw190/mfa/config/JwtService.java
@@ -1,14 +1,42 @@
 package com.davidandw190.mfa.config;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    @Value("${jwt.secret}")
+    private static String SECRET_KEY;
 
     public String extractUsername(String token) {
-
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 
