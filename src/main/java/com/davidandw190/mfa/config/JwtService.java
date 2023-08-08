3ccc5fafb218d509808b85fed7b03d085a34b008
Andrei-David Nan <nandavid190@gmail.com>
2023-08-08 18:28:41 +0300

    v0.5 -  set up token generation and the rest of the needed methods in JwtService class, added java docs to them;

--- src/main/java/com/davidandw190/mfa/config/JwtService.java
+++ src/main/java/com/davidandw190/mfa/config/JwtService.java
@@ -2,32 +2,89 @@ package com.davidandw190.mfa.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
+/**
+ * Service class for handling JWT (JSON Web Token) related operations.
+ */
 @Service
 public class JwtService {
+
     @Value("${jwt.secret}")
     private static String SECRET_KEY;
 
-    public String extractUsername(String token) {
-        return null;
+    /**
+     * Generates a JWT token with the provided extra claims and user details.
+     *
+     * @param extraClaims Additional claims to be included in the token payload.
+     * @param userDetails UserDetails of the user for whom the token is being generated.
+     * @return Generated JWT token as a String.
+     */
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24 * 3)) // Token expiration: 3 days
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
+    /**
+     * Extracts a specific claim from the provided JWT token.
+     *
+     * @param token          JWT token from which to extract the claim.
+     * @param claimsResolver Function that resolves the desired claim from the token's claims.
+     * @param <T>            Type of the claim value.
+     * @return The extracted claim value.
+     */
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
+    /**
+     * Validates whether a JWT token is valid for the given user details.
+     *
+     * @param token       JWT token to be validated.
+     * @param userDetails UserDetails of the user against which to validate the token.
+     * @return True if the token is valid for the user, otherwise false.
+     */
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    /**
+     * Extracts the username from a JWT token.
+     *
+     * @param token JWT token from which to extract the username.
+     * @return Extracted username.
+     */
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 
     private Claims extractAllClaims(String token) {
-        return Jwts
-                .parserBuilder()
+        return Jwts.parserBuilder()
                 .setSigningKey(getSigningKey())
                 .build()
                 .parseClaimsJws(token)
@@ -38,6 +95,4 @@ public class JwtService {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
-
-
 }
