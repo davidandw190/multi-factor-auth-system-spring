c8f9d36ee59736f218466696d9e215255f38ea6b
Andrei-David Nan <nandavid190@gmail.com>
2023-08-08 12:55:25 +0300

    v0.1 - initial commit;

---
+++ src/main/java/com/davidandw190/mfa/MfaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.davidandw190.mfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidandw190/mfa/MfaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidandw190.mfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
