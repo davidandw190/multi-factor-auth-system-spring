4c087e9d40de670cc8f2ed0f0471a0707eb3ce92
Andrei-David Nan <nandavid190@gmail.com>
2023-08-09 13:32:25 +0300

    v0.9 -  configured authenticationProvider, authenticationManager and encoder beans ;

--- src/main/java/com/davidandw190/mfa/config/ApplicationConfig.java
+++ src/main/java/com/davidandw190/mfa/config/ApplicationConfig.java
@@ -3,8 +3,14 @@ package com.davidandw190.mfa.config;
 import com.davidandw190.mfa.repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfig {
@@ -21,6 +27,24 @@ public class ApplicationConfig {
         return username -> userRepository.findUserByEmail(username).orElseThrow(
                 () -> new UsernameNotFoundException("No user found with email: " + username)
         );
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
